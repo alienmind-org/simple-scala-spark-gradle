121b74921b3cf2968b56bd27f363c373e1fc9cb0
yanggao1119 <yanggao@Yangs-MacBook.local>
2020-07-27 23:28:55 -0400

    first commit

---
+++ src/main/scala/com/tutorial/spark/SimpleSparkJob.scala
@@ -0,0 +1,15 @@
+package com.tutorial.spark
+
+import org.apache.spark.sql.SparkSession
+
+object SimpleSparkJob {
+  def main(args: Array[String]) {
+    val logFile = "/Users/yanggao/tools/spark-3.0.0-bin-hadoop2.7/README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
