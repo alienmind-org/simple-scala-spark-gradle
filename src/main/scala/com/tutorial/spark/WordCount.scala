2844dbc4f61fa86201686c5df2062f29d80b5dfd
yanggao1119 <yanggao@Yangs-MacBook.local>
2020-08-02 17:22:15 -0400

    remove-textFile-input

--- src/main/scala/com/tutorial/spark/WordCount.scala
+++ src/main/scala/com/tutorial/spark/WordCount.scala
@@ -4,12 +4,11 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]) {
-    // adapted from wiki: https://en.wikipedia.org/wiki/Apache_Spark
-    val conf = new SparkConf().setAppName("wiki_test") // create a spark config object
+    // simplified from wiki: https://en.wikipedia.org/wiki/Apache_Spark
+    val conf = new SparkConf().setAppName("wiki_test").setMaster("local") // create a spark config object
     val sc = new SparkContext(conf) // Create a spark context
-    val data = sc.textFile("/Users/yanggao/tools/spark-3.0.0-bin-hadoop2.7/README.md") // Read files from "somedir" into an RDD of (filename, content) pairs.
-    val tokens = data.flatMap(_.split(" ")) // Split each file into a list of tokens (words).
-    val wordFreq = tokens.map((_, 1)).reduceByKey(_ + _) // Add a count of one to each token, then sum the counts per word type.
+    val lines = Array("a b c", "a b")
+    val wordFreq = sc.parallelize(lines).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
     wordFreq.sortBy(s => -s._2).map(x => (x._2, x._1)).top(10) // Get the top 10 words. Swap word and count to sort by count.
     println(s"================\nword frequency as map: ${wordFreq.collectAsMap().toString()}")
   }
