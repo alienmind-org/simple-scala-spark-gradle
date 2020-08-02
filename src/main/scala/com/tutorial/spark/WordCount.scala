69e06068d0475aa545f8773283677c2c6e9876e8
yanggao1119 <yanggao@Yangs-MacBook.local>
2020-08-02 17:24:40 -0400

    minor

--- src/main/scala/com/tutorial/spark/WordCount.scala
+++ src/main/scala/com/tutorial/spark/WordCount.scala
@@ -5,7 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount {
   def main(args: Array[String]) {
     // simplified from wiki: https://en.wikipedia.org/wiki/Apache_Spark
-    val conf = new SparkConf().setAppName("wiki_test").setMaster("local") // create a spark config object
+    val conf = new SparkConf().setAppName("wiki_test") // create a spark config object
     val sc = new SparkContext(conf) // Create a spark context
     val lines = Array("a b c", "a b")
     val wordFreq = sc.parallelize(lines).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
